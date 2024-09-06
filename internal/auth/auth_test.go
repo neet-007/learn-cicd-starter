01d282617adb82bfa147675bf021327fc9bc43ab
neet-007 <moayedeffect@gmail.com>
2024-09-06 14:51:12 +0300

    broken tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,50 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	testHeader1 := http.Header{}
+	testHeader2 := http.Header{}
+	testHeader3 := http.Header{}
+	testHeader4 := http.Header{}
+	testHeader1.Set("", "")
+	testHeader2.Set("Authorization", "")
+	testHeader3.Set("Authorization", "teset api")
+	testHeader4.Set("Authorization", "ApiKey correctke")
+	cases := []struct {
+		input    http.Header
+		expected struct {
+			key string
+			err error
+		}
+	}{
+		{input: testHeader1, expected: struct {
+			key string
+			err error
+		}{key: "", err: ErrNoAuthHeaderIncluded}},
+		{input: testHeader2, expected: struct {
+			key string
+			err error
+		}{key: "", err: errors.New("malformed authorization header")}},
+		{input: testHeader2, expected: struct {
+			key string
+			err error
+		}{key: "", err: errors.New("malformed authorization header")}},
+		{input: testHeader4, expected: struct {
+			key string
+			err error
+		}{key: "correctkey", err: nil}},
+	}
+
+	for _, testCase := range cases {
+		actual, err := GetAPIKey(testCase.input)
+
+		if actual != testCase.expected.key && err != testCase.expected.err {
+			t.Errorf("expected key: %v, got: %v, expected err: %v, got: %v", testCase.expected.key, actual, testCase.expected.err, err)
+		}
+	}
+}
